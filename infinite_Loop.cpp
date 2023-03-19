07dd757c5d09edc1545dcc9fffc7223ce9c6ca96
Mohammad Aazen <mohdaazen@gmail.com>
2023-03-19 17:02:16 +0530

    Learned infinite loop using different syntaxes

--- infinite_Loop.cpp
+++ infinite_Loop.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>  //this is optional
+#include <vector>             //this is optional
 using namespace std;
 
 int main()
@@ -7,5 +7,51 @@ int main()
 for(int i{1}; ;i++){
 cout<<i<<endl;
 }
+    return 0;
+}
+
+
+
+
+                             //Infinite loop using while statement
+
+
+
+#include <iostream>
+#include <vector>  //this is optional
+using namespace std;
+
+int main()
+{
+int i{1};
+bool done{false};
+while(true){
+cout<<i<<endl;
+    i++;
+}
+    return 0;
+}
+
+
+
+
+
+
+                              //Infinite loop using do while statement
+
+
+
+#include <iostream>
+#include <vector>  //this is optional
+using namespace std;
+
+int main()
+{
+int i{1};
+bool done{false};
+do{
+cout<<i<<endl;
+    i++;
+}while(done=true);
     return 0;
 }
