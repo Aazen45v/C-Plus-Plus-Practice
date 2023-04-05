56661dce9b8242cd525cede1a6f903d21e72e260
Mohammad Aazen <mohdaazen@gmail.com>
2023-04-05 22:18:50 +0530

    Generalized  solution of Inverted half numeric pyramid

---
+++ Inverted half numeric pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"Enter number of rows to make half pyramid: ";
+  cin>>n;
+  for(int row=0;row<n;row++){
+    for(int col=0;col<n-row;col++){
+      cout<<col+1;
+    }
+    cout<<endl;
+  }
+  return 0;
+}
