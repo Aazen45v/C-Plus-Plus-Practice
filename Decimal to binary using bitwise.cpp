d5917efbf2ee70a9a0768810e3912ae1932d7d39
Mohammad Aazen <mohdaazen@gmail.com>
2023-04-18 16:26:01 +0530

    Decimal to binary using bitwise method

---
+++ Decimal to binary using bitwise.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int decimalToBinary(int n){
+  int binaryNo = 0;
+  int i = 0;
+  while(n>0){
+    int bit=(n&1);
+    binaryNo = bit*pow(10,i++) + binaryNo;
+    n=n >> 1;
+  }
+  return binaryNo;
+}
+
+
+
+int main(){
+  int n;
+  cin>>n;
+  int binary=decimalToBinary(n);
+  cout<<binary<<endl;
+return 0;
+}
