487fba16f00bf3f87998f9d2300a7b7c903f508f
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-05 00:23:36 +0530

    Update Smallest number in an array.cpp
    Smallest number is: 1

--- Smallest number in an array.cpp
+++ Smallest number in an array.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main() {
   int arr[]={1,8,7,20,15,18,23,4,89,48,55,78};
   int size=12;
-  int max = INT_MAX;
+  int min = INT_MAX;
 
   for(int i=0;i<size;i++){
-    if(arr[i]<max){
-      max=arr[i];
+    if(arr[i]<min){
+      min=arr[i];
     }
   }
-  cout<<"Smallest number is: "<<max<<endl;
+  cout<<"Smallest number is: "<<min<<endl;
 }
