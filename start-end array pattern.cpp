014689c733230d5fe8545842f744cb99c4b4ac83
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-05 00:45:56 +0530

    Update start-end array pattern.cpp
    1 55 8 48 7 89 20 4 15 23 18

--- start-end array pattern.cpp
+++ start-end array pattern.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main() {
-  int arr[]={1,8,7,20,15,18,23,4,89,48,55,78};
-  int size=12;
+  int arr[]={1,8,7,20,15,18,23,4,89,48,55};
+  int size=11;
   int start = 0;
   int end = size-1;
 
   while(true){
     if(start>end)
       break;
+    if(start==end){
+    cout<<arr[start]<<" ";
+    } else {
     cout<<arr[start]<<" ";
     cout<<arr[end]<<" ";
+    }
     start++;
     end--;
   }
