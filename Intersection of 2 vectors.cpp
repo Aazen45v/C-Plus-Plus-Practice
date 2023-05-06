32854f0733a4b09f3a5baf8ed34adf1f4a1440d9
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-06 18:50:29 +0530

    Create Intersection of 2 vectors.cpp
    8 15

---
+++ Intersection of 2 vectors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector <int> arr{1,8,7,20,15,18,23,4,89,48,55};
+  vector <int> brr{100,8,103,104,15};
+  vector <int> ans;
+
+  for(int i=0;i<arr.size();i++){
+    int element=arr[i];
+
+  for(int j=0;j<brr.size();j++){
+    if(element==brr[j]){
+      ans.push_back(element);
+      }
+    }
+  }
+
+  for(int k=0;k<ans.size();k++){
+    cout<<ans[k]<<" ";
+  }
+  cout<<endl;
+}
