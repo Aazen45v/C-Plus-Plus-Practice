5261cfa261299f72eccc55fc7916ff3620320ee4
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-06 20:58:40 +0530

    Update sum of 3 pairs in a vector.cpp
    Enter the number to finds pairs that result in the sum:80
    (7,18,55)(20,30,30)

--- sum of 3 pairs in a vector.cpp
+++ sum of 3 pairs in a vector.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-  vector <int> arr{1,8,7,20,15,18,23,4,48,55,20,30,13,30};
+  vector <int> arr{1,8,7,20,15,18,23,4,48,55,13,30};
   int sum;
   cout<<"Enter the number to finds pairs that result in the sum:";
   cin>>sum;
@@ -11,13 +11,13 @@ int main() {
   for(int i=0;i<arr.size();i++){
     int element=arr[i];
 
-    for(int j=1;j<arr.size();j++){
+    for(int j=i+1;j<arr.size();j++){
       int element2=arr[j];
 
-      for(int k=0;k<arr.size();k++){
+      for(int k=i+2;k<arr.size();k++){
         if(element+element2+arr[k]==sum){
-          cout<<"("<<element<<","<<arr[j]<<","<<arr[k]<<")";
-            arr[j]=-1;      
+          cout<<"("<<element<<","<<element2<<","<<arr[k]<<")";
+            arr[k]=-1;      
         }
       }
     }
