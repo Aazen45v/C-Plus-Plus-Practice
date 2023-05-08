7312dcc4873d5b59b9570c37ff1bb4ede79f19da
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-08 18:49:55 +0530

    Create Initializing 2D array.cpp
    The element is: 8

---
+++ Initializing 2D array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  //declaring and initializing a 2d array
+  int arr[3][3]=
+  //[row][array]
+  {
+  {2,3,4},
+  {5,6,7},
+  {8,0,9}
+  };
+  //printing out the element
+  cout<<"The element is: "<<arr[2][0];
+}
