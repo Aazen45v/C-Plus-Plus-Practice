f6e3165eea66340f87eb6377d5050f8d77b79e8c
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-08 19:33:05 +0530

    Create Entering elements in a 2D array(row-wise or column-wise).cpp
    Taking input of elements in row-wise form
    5
    6
    7
    
    2
    3
    7
    
    8
    5
    2
    
    printing out the elements in row-wise form
     5 6 7
     2 3 7
     8 5 2
    
    printing out the elements in column-wise form
     5 2 8
     6 3 5
     7 7 2

---
+++ Entering elements in a 2D array(row-wise or column-wise).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  //declaring and initializing a 2d array
+  int arr[3][3];
+      //[row][array]
+
+  //taking inout of elements in row-wise form
+  cout<<"Taking input of elements in row-wise form"<<endl;
+  for(int row=0;row<3;row++){
+    for(int col=0;col<3;col++){
+  cin>>arr[row][col];
+    }
+    cout<<endl;
+  }
+
+  
+  // //taking inout of elements in column-wise form
+  // cout<<"Taking input of elements in column-wise form"<<endl;
+  // for(int row=0;row<3;row++){
+  //   for(int col=0;col<3;col++){
+  //     cin>>arr[col][row];
+  //   }
+  //   cout<<endl;
+  // }
+
+  
+  //printing out the elementsin row-wise form
+  cout<<"printing out the elements in row-wise form"<<endl;
+  for(int row=0;row<3;row++){
+    for(int col=0;col<3;col++){
+  cout<<" "<<arr[row][col];
+    }
+    cout<<endl;
+  }
+cout<<endl;
+  //printing out the elements in column-wise form
+  cout<<"printing out the elements in column-wise form"<<endl;
+  for(int row=0;row<3;row++){
+    for(int col=0;col<3;col++){
+      cout<<" "<<arr[col][row];
+    }
+    cout<<endl;
+  }
+  cout<<endl;
+  }
