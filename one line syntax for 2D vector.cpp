878d9fbd524895e62ab507d8f3116afbb57bad3e
Mohammad Aazen <mohdaazen@gmail.com>
2023-05-09 16:56:00 +0530

    Create one line syntax for 2D vector.cpp
    -8-8-8-8-8
    -8-8-8-8-8
    -8-8-8-8-8
    -8-8-8-8-8
    -8-8-8-8-8

---
+++ one line syntax for 2D vector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+
+vector<vector<int> > arr(5, vector<int>(5,-8));
+
+  
+    for(int i=0;i<arr.size();i++){
+    for(int j=0;j<arr[i].size();j++){
+      cout<<arr[i][j];
+    }
+      cout<<endl;
+  }
+}
