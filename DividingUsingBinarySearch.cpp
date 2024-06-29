1cc05680c1e8eeec41f63901a7dda48660116b83
Mohammad Aazen <mohdaazen@gmail.com>
2024-06-29 15:12:31 +0530

    ErrorFixed DividingUsingBinarySearch.cpp
    22/7
    Quotient is : 3
    22/-7
    Quotient is : -3

--- DividingUsingBinarySearch.cpp
+++ DividingUsingBinarySearch.cpp
@@ -7,25 +7,30 @@ int quotient(int dividend, int divisor) {
   int ans = 0;
   int start = 0;
   int end = dividend;
-  
+  int mid = start + (end - start) / 2;
+
   while (start <= end) {
-    int mid = start + (end - start) / 2;
-    
-    if (mid * divisor == dividend) {
+    if (abs(mid * divisor) == abs(dividend)) {
       return mid;
-    } else if (mid * divisor < dividend) {
-      ans = mid;  // store the potential answer
+    } else if (abs(mid * divisor) < abs(dividend)) {
+      ans = mid;
       start = mid + 1;
     } else {
       end = mid - 1;
     }
+    mid = start + (end - start) / 2;
   }
-  return ans;
+
+  if ((divisor < 0 && dividend < 0) || (divisor > 0 && dividend > 0)) {
+    return ans;
+  } else {
+    return -ans;
+  }
 }
 
 int main() {
   int dividend = 22;
-  int divisor = 7;
+  int divisor = -7;
 
   int index = quotient(dividend, divisor);
   cout << "Quotient is : " << index << endl;
