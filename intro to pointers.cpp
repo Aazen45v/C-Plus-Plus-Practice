23da89dba9b4a6cb1aa64891bfdc3213981e81d1
Mohammad Aazen <mohdaazen@gmail.com>
2024-08-01 22:50:54 +0530

    Update intro to pointers.cpp
    Intro to pointers

--- intro to pointers.cpp
+++ intro to pointers.cpp
@@ -8,5 +8,6 @@ int main() {
   int *ptr =&a;
   cout<<*ptr<<endl; //To get the value stored in the address
   cout<<ptr<<endl; // To get the address of the variable
+  cout<<&ptr<<endl; //address value of the box ptr
 return 0;
 }
